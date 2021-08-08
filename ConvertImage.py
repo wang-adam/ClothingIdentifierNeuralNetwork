6a80cd9d38d98b479d5975489f5133cdce80217f
adamwang <adamw43081@gmail.com>
2021-08-08 13:45:11 -0700

    fixed external image predictor.

--- ConvertImage.py
+++ ConvertImage.py
@@ -1,13 +1,9 @@
-from PIL import Image
-from matplotlib import cm
 import numpy as np
-import math
 import cv2
 import matplotlib.pyplot as plt
 
-from numpy.core.fromnumeric import mean
 
-
+# Compresses the given image down to a 28x28 array, which the Neural Network uses.
 def compress(fileName):
     data = cv2.imread(fileName, cv2.IMREAD_GRAYSCALE)/255
     # test = np.arange(0, 100, 1).reshape((10, 10))
@@ -15,15 +11,12 @@ def compress(fileName):
     arraySize = 28
     row = int(np.floor(data.shape[0]/arraySize))
     col = int(np.floor(data[0].shape[0]/arraySize))
-    newData = np.zeros((arraySize, arraySize))
+    newData = np.zeros((1, arraySize, arraySize))
     for i in range(arraySize):
         for j in range(arraySize):
-            newData[i, j] = np.sum(
+            newData[0, i, j] = np.sum(
                 data[i*row:(i+1)*row, j*col:(j+1)*col])/(row*col)
     # plt.imshow(1-newData)
     # plt.colorbar()
     # plt.show()
     return 1-newData
-
-
-# compress("shoe.jpg")
--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -3,35 +3,52 @@ import numpy as np
 import matplotlib.pyplot as plt
 import ConvertImage
 
+# Image to test Neural Network on. It gets compressed into a 28x28 matrix
+this_image = ConvertImage.compress("shoe.jpg")
+
+# Dataset to train the network on.
 fashion_mnist = tf.keras.datasets.fashion_mnist
 
 (train_images, train_labels), (test_images,
                                test_labels) = fashion_mnist.load_data()
 
+# We want all the values to be between 0 and 1.0
 train_images = train_images / 255.0
-
 test_images = test_images / 255.0
 
+# Category Names
 class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
                'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 
+
+# Model consists of a layer to flatten the matrix, 3 Dense layers with 128 neurons each, and then the output layer
 model = tf.keras.Sequential([
     tf.keras.layers.Flatten(input_shape=(28, 28)),
+    tf.keras.layers.Dense(128, activation='relu'),
     tf.keras.layers.Dense(128, activation='relu'),
     tf.keras.layers.Dense(128, activation='relu'),
     tf.keras.layers.Dense(10)
 ])
 
-model.compile(optimizer='adam',
-              loss=tf.keras.losses.SparseCategoricalCrossentropy(
-                  from_logits=True),
-              metrics=['accuracy'])
 
-model.fit(train_images, train_labels, epochs=20)
+# Load the model from disk or train the model if none:
+try:
+    model.load_weights('model.weights')
+    print("Model Weights Loaded")
+except:
+    model.compile(optimizer='adam',
+                  loss=tf.keras.losses.SparseCategoricalCrossentropy(
+                      from_logits=True),
+                  metrics=['accuracy'])
+    print("Training Model...")
+    # Fit the model with the training data
+    model.fit(train_images, train_labels, epochs=20)
+    # Save the model to disk.
+    model.save_weights('model.weights')
 
 
-def plot_image(i, predictions_array, true_label, img):
-    true_label, img = true_label[i], img[i]
+# Plots the Image
+def plot_image(predictions_array, img):
     plt.grid(False)
     plt.xticks([])
     plt.yticks([])
@@ -39,53 +56,36 @@ def plot_image(i, predictions_array, true_label, img):
     plt.imshow(img, cmap=plt.cm.binary)
 
     predicted_label = np.argmax(predictions_array)
-    if predicted_label == true_label:
-        color = 'blue'
-    else:
-        color = 'red'
+    color = 'blue'
 
-    plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label],
-                                         100*np.max(predictions_array),
-                                         class_names[true_label]),
+    plt.xlabel("{} {:2.0f}%".format(class_names[predicted_label],
+                                    100*np.max(predictions_array)),
                color=color)
 
 
-def plot_value_array(i, predictions_array, true_label):
-    true_label = true_label[i]
+# Plots the predictions
+def plot_value_array(predictions_array):
     plt.grid(False)
-    plt.xticks(range(10))
+    plt.xticks(range(10), class_names, rotation=90)
     plt.yticks([])
     thisplot = plt.bar(range(10), predictions_array, color="#777777")
     plt.ylim([0, 1])
     predicted_label = np.argmax(predictions_array)
-
-    thisplot[predicted_label].set_color('red')
-    thisplot[true_label].set_color('blue')
-
-
-this_image = ConvertImage.compress("shirt.jpg")
-
-test_images[0] = this_image
+    thisplot[predicted_label].set_color('blue')
 
 
 # test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
 probability_model = tf.keras.Sequential([model,
                                          tf.keras.layers.Softmax()])
 
-predictions = probability_model.predict(test_images)
-# Plot the first X test images, their predicted labels, and the true labels.
-# Color correct predictions in blue and incorrect predictions in red.
-num_rows = 10
-num_cols = 4
-num_images = num_rows*num_cols
-plt.figure(figsize=(2*2*num_cols, 2*num_rows))
-for i in range(num_images):
-    plt.subplot(num_rows, 2*num_cols, 2*i+1)
-    plot_image(i, predictions[i], test_labels, test_images)
-    plt.subplot(num_rows, 2*num_cols, 2*i+2)
-    plot_value_array(i, predictions[i], test_labels)
+predictions = probability_model.predict(this_image)
+
+# Plot the first X test images and their predicted labels.
+
+
+plt.subplot(1, 2, 1)
+plot_image(predictions[0], this_image[0])
+plt.subplot(1, 2, 2)
+plot_value_array(predictions[0])
 plt.tight_layout()
 plt.show()
-
-
-# print('\nTest accuracy:', test_acc)
